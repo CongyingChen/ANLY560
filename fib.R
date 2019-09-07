b46905f71a0b149b9e306f44216de1b13d46745b
Charlene Chen <31579611+CongyingChen@users.noreply.github.com>
2019-09-07 15:01:43 -0400

    Add files via upload

---
+++ fib.R
@@ -0,0 +1,7 @@
+#Clear environment
+#Numeric
+a=c(2,3,4,5)
+class(a)
+#Character
+b=c("Jim","Bob","kate","Tom")
+class(b)
---
+++ fib.py
@@ -0,0 +1,35 @@
+import plotly.plotly as py
+import pandas as pd
+import numpy as np
+import plotly.graph_objs as go
+import plotly
+import plotly.graph_objs as go
+from plotly.offline import *
+import colorlover as cl
+from IPython.display import HTML
+import os
+import argparse
+import time
+import pandas as pd
+
+import seaborn as sns; sns.set()
+import matplotlib.pyplot as plt
+import itertools
+
+from sklearn import preprocessing
+import warnings
+from datetime import datetime
+import timeit
+import math
+import pytz
+from datetime import datetime
+import calendar
+#from datetime import *
+
+import datetime
+import pytz
+import statistics
+from dateutil.relativedelta import relativedelta
+
+from bokeh.plotting import figure
+from bokeh.io import show, output_notebook
